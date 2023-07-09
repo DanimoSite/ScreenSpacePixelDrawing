24a456d7a82b2ff8c38ac9cc5353332cdf4bc47f
Ryan Tan <65996005+ryn5@users.noreply.github.com>
2023-07-08 20:53:24 -0700

    Add MenuTheme and adjust menu

---
+++ Assets/Scripts/MenuTheme.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuTheme : MonoBehaviour
+{
+    private static MenuTheme menuTheme;
+    void Awake()
+    {
+        if (menuTheme == null)
+        {
+            menuTheme = this;
+        }
+    }
+}
