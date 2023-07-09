a0682213cdb715a6ca0d293d76eeb7c84f081ca5
Ryan Tan <65996005+ryn5@users.noreply.github.com>
2023-07-08 21:16:10 -0700

    Set initial build to MenuScene and group menu scripts

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
